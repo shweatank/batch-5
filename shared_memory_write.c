6d26aeeb47150b0737752ed6f99aa7c47966093a
nagaprasannakoritala <nagaprasannakoritala@mirafra.com>
2026-01-21 09:42:04 +0530

    shm

---
+++ shared_memory_write.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+int main()
+{
+	int id=shmget(9,20,IPC_CREAT|0664);
+	if(id<0)
+	{
+		perror("shmget");
+		return 0;
+	}
+	printf("id:%d\n",id);
+	int *ptr=shmat(id,0,0);
+	*ptr=5;
+	shmdt(ptr);
+}
