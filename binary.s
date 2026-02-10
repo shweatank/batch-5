abdf96fa71b95239afcbd6250b7209c1a4e79b11
abhishekpadala005 <abhishekpadala@mirafra.com>
2026-02-10 09:07:22 +0530

    new

---
+++ binary.s
@@ -0,0 +1,109 @@
+	.file	"binary.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Enter a binary number: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"Decimal value: %d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -20(%rbp)
+	movl	$1, -16(%rbp)
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	jmp	.L2
+.L3:
+	movl	-24(%rbp), %edx
+	movslq	%edx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	sarl	$2, %eax
+	movl	%edx, %ecx
+	sarl	$31, %ecx
+	subl	%ecx, %eax
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %ecx
+	movl	%ecx, %eax
+	sall	$2, %eax
+	addl	%ecx, %eax
+	addl	%eax, %eax
+	subl	%eax, %edx
+	movl	%edx, -12(%rbp)
+	movl	-12(%rbp), %eax
+	imull	-16(%rbp), %eax
+	addl	%eax, -20(%rbp)
+	sall	-16(%rbp)
+	movl	-24(%rbp), %eax
+	movslq	%eax, %rdx
+	imulq	$1717986919, %rdx, %rdx
+	shrq	$32, %rdx
+	movl	%edx, %ecx
+	sarl	$2, %ecx
+	cltd
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	movl	%eax, -24(%rbp)
+.L2:
+	movl	-24(%rbp), %eax
+	testl	%eax, %eax
+	jg	.L3
+	movl	-20(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
