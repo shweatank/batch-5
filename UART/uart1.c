51d10218a245cc42c80d55b1210a2ffae2e60613
jugalkishore813 <jugalkishore@mirafra.com>
2026-02-12 16:49:01 +0530

    new

--- UART/uart1.c
+++ UART/uart1.c
@@ -50,111 +50,117 @@ static void __iomem *uart_base;
 /* ---------------- UART INIT ---------------- */
 static void uart_hw_init(void)
 {
-    uart_write(0, UART_CR);        /* Disable UART */
-    uart_write(0x7FF, UART_ICR);   /* Clear IRQs */
+	uart_write(0, UART_CR);        /* Disable UART */
+	uart_write(0x7FF, UART_ICR);   /* Clear IRQs */
 
-    /* 115200 baud @ 48MHz */
-    uart_write(26, UART_IBRD);
-    uart_write(3,  UART_FBRD);
+	/* 115200 baud @ 48MHz */
+	uart_write(26, UART_IBRD);
+	uart_write(3,  UART_FBRD);
 
-    uart_write(LCRH_8BIT | LCRH_FEN, UART_LCRH);
+	uart_write(LCRH_8BIT | LCRH_FEN, UART_LCRH);
 
-    uart_write(CR_UARTEN | CR_TXE | CR_RXE, UART_CR);
-    uart_write(0, UART_IMSC);      /* No interrupts */
+	uart_write(CR_UARTEN | CR_TXE | CR_RXE, UART_CR);
+	uart_write(0, UART_IMSC);      /* No interrupts */
 
-    pr_info("rpi_uart: UART initialized\n");
+	pr_info("rpi_uart: UART initialized\n");
 }
 
+static void uart_send(char ch){
+
+	while(uart_read(UART_FR)&FR_TXFF)
+		cpu_relax();
+	uart_write(ch,UART_DR);
+}
+
+static char uart_recv(void){
+
+	if(uart_read(UART_FR)&FR_RXFE)
+		return -EAGAIN;
+	return uart_read(UART_DR)&0xFF;
+}
 /* ---------------- FILE OPS ---------------- */
 static ssize_t uart_write_user(struct file *f,
-                               const char __user *buf,
-                               size_t len, loff_t *off)
+		const char __user *buf,
+		size_t len, loff_t *off)
 {
-    char ch;
-    size_t i;
+	char ch;
+	size_t i;
 
-    for (i = 0; i < len; i++) {
-        if (copy_from_user(&ch, buf + i, 1))
-            return -EFAULT;
+	for (i = 0; i < len; i++) {
+		if (copy_from_user(&ch, buf + i, 1))
+			return -EFAULT;
+		uart_send(ch);
 
-        while (uart_read(UART_FR) & FR_TXFF)
-            cpu_relax();
-
-        uart_write(ch, UART_DR);
-    }
-    return len;
+	}
+	return len;
 }
 
 static ssize_t uart_read_user(struct file *f,
-                              char __user *buf,
-                              size_t len, loff_t *off)
+		char __user *buf,
+		size_t len, loff_t *off)
 {
-    char ch;
-    size_t i;
+	char ch;
+	size_t i;
 
-    for (i = 0; i < len; i++) {
-        if (uart_read(UART_FR) & FR_RXFE)
-            break;
-
-        ch = uart_read(UART_DR) & 0xFF;
-
-        if (copy_to_user(buf + i, &ch, 1))
-            return -EFAULT;
-    }
-    return i;
+	for (i = 0; i < len; i++) {
+		ch=uart_recv();
+		if (copy_to_user(buf + i, &ch, 1))
+			return -EFAULT;
+	}
+	return i;
 }
 
 static struct file_operations uart_fops = {
-    .owner = THIS_MODULE,
-    .read  = uart_read_user,
-    .write = uart_write_user,
+	.owner = THIS_MODULE,
+	.read  = uart_read_user,
+	.write = uart_write_user,
 };
 
 /* ---------------- MODULE INIT ---------------- */
 static int __init uart_init(void)
 {
-    int ret;
+	int ret;
 
-    uart_base = ioremap(UART_BASE, UART_SIZE);
-    if (!uart_base) {
-        pr_err("rpi_uart: ioremap failed\n");
-        return -ENOMEM;
-    }
+	uart_base = ioremap(UART_BASE, UART_SIZE);
+	if (!uart_base) {
+		pr_err("rpi_uart: ioremap failed\n");
+		return -ENOMEM;
+	}
 
-    ret = alloc_chrdev_region(&devno, 0, 1, DEVICE_NAME);
-    if (ret)
-        goto unmap;
+	ret = alloc_chrdev_region(&devno, 0, 1, DEVICE_NAME);
+	if (ret)
+		goto unmap;
 
-    cdev_init(&uart_cdev, &uart_fops);
-    ret = cdev_add(&uart_cdev, devno, 1);
-    if (ret)
-        goto unregister;
+	cdev_init(&uart_cdev, &uart_fops);
+	ret = cdev_add(&uart_cdev, devno, 1);
+	if (ret)
+		goto unregister;
 
-    uart_class = class_create(DEVICE_NAME);
-    device_create(uart_class, NULL, devno, NULL, DEVICE_NAME);
+	uart_class = class_create(DEVICE_NAME);
+	device_create(uart_class, NULL, devno, NULL, DEVICE_NAME);
 
-    uart_hw_init();
+	uart_hw_init();
 
-    pr_info("rpi_uart: loaded (ioremap only)\n");
-    return 0;
+	pr_info("rpi_uart: loaded (ioremap only)\n");
+	return 0;
 
 unregister:
-    unregister_chrdev_region(devno, 1);
+	unregister_chrdev_region(devno, 1);
 unmap:
-    iounmap(uart_base);
-    return ret;
+	iounmap(uart_base);
+	return ret;
 }
 
 /* ---------------- MODULE EXIT ---------------- */
 static void __exit uart_exit(void)
 {
-    device_destroy(uart_class, devno);
-    class_destroy(uart_class);
-    cdev_del(&uart_cdev);
-    unregister_chrdev_region(devno, 1);
-    iounmap(uart_base);
+	device_destroy(uart_class, devno);
+	class_destroy(uart_class);
+	cdev_del(&uart_cdev);
+	unregister_chrdev_region(devno, 1);
+	iounmap(uart_base);
 
-    pr_info("rpi_uart: unloaded\n");
+	pr_info("rpi_uart: unloaded\n");
 }
 
 module_init(uart_init);
