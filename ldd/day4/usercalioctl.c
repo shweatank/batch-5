65f85f7a16eee934c22b7388028cb815488c5e36
likithakalisetti <likithakalisetti@mirafra.com>
2026-02-26 11:55:15 +0530

    ldd

---
+++ ldd/day4/usercalioctl.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+        char s[]="30-15";
+        FILE *fd=fopen("/dev/calc_ioctl","w+");
+        fprintf(fd,"%s",s);
+        printf("Before:%s\n",s);
+        fscanf(fd,"%s",s);
+        printf("After:%s",s);
+}
+ 
+
