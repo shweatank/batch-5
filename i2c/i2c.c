89d1a34d41d0e6f51fe0c49e762620a7d7233a59
saikumarbadana23 <saikumarbadana@mirafra.com>
2026-02-26 14:34:27 +0530

    Add files via upload

---
+++ i2c/i2c.c
@@ -0,0 +1,9 @@
+#define BASE_PHYS 0xfe205000
+#define C 0x00
+
+
+
+
+#define I2CEN 15
+#define READ 0
+#define ST 7
---
+++ i2c/i2c_dts_driver.mod.c
@@ -0,0 +1,47 @@
+#include <linux/module.h>
+#include <linux/export-internal.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0xfa474811, "__platform_driver_register" },
+	{ 0xdf484963, "device_destroy" },
+	{ 0x6775d5d3, "class_destroy" },
+	{ 0x6bc3fbc0, "__unregister_chrdev" },
+	{ 0x85a4029c, "i2c_unregister_device" },
+	{ 0x92997ed8, "_printk" },
+	{ 0x61fd46a9, "platform_driver_unregister" },
+	{ 0x44b05684, "i2c_transfer_buffer_flags" },
+	{ 0xf0fdf6cb, "__stack_chk_fail" },
+	{ 0x12a4e128, "__arch_copy_from_user" },
+	{ 0xdcb764ad, "memset" },
+	{ 0x534e9a04, "i2c_get_adapter" },
+	{ 0xceb97bfc, "i2c_new_dummy_device" },
+	{ 0x4080a4df, "i2c_put_adapter" },
+	{ 0xf9a482f9, "msleep" },
+	{ 0x418c10ec, "__register_chrdev" },
+	{ 0x59c02473, "class_create" },
+	{ 0xb63fdcdb, "device_create" },
+	{ 0x474e54d2, "module_layout" },
+};
+
+MODULE_INFO(depends, "");
+
+MODULE_ALIAS("of:N*T*Cmy,i2c");
+MODULE_ALIAS("of:N*T*Cmy,i2cC*");
+
+MODULE_INFO(srcversion, "1D81DF8FC50AB8549DE7C97");
